c31c7e677f8bd588d39a39662adeda631653bf98
Melissa Kepler <mmkepler@yahoo.com>
2019-08-01 17:44:26 -0400

    fix: added data for stores

---
+++ src/components/location-data/StoreLocations.js
@@ -0,0 +1,676 @@
+const StoreLocations = [
+  {
+    'neighborhood': 'Buckhead',
+    'brand': 'Petsmart',
+    'address': '3221 Peachtree Rd NE, Atlanta, GA 30305',
+    'lat': 33.841473,
+    'lng': -84.372974,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0287',
+    'phone': '(404) 266-0402',
+    'id': '0'
+  },
+  {
+    'neighborhood': 'Perimeter',
+    'brand': 'Petsmart',
+    'address': '128 Perimeter Center W, Atlanta, GA 30346',
+    'lat': 33.926568,
+    'lng': -84.346893,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0290',
+    'phone': '(770) 481-0043',
+    'id': '1'
+  },
+  {
+    'neighborhood': 'Smyrna',
+    'brand': 'Petsmart',
+    'address': '2540 Cumberland Blvd, Smyrna, GA 30080',
+    'lat': 33.888548,
+    'lng': -84.477732,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0291',
+    'phone': '(770) 432-8250',
+    'id': '2'
+  },
+  {
+    'neighborhood': 'Castlegate',
+    'brand': 'Petsmart',
+    'address': '1801 Howell Mill Rd NW Ste 30, Atlanta, GA 30318',
+    'lat': 33.803135,
+    'lng': -84.411604,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=1571',
+    'phone': '(404) 352-9746',
+    'id': '3'
+  },
+  {
+    'neighborhood': 'Marietta',
+    'brand': 'Petsmart',
+    'address': '1285 Johnson Ferry Rd, Marietta, GA 30068',
+    'lat': 33.978896,
+    'lng': -84.424928,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0286',
+    'phone': '(770) 971-3010',
+    'id': '4'
+  },
+  {
+    'neighborhood': 'Tucker',
+    'brand': 'Petsmart',
+    'address': '4023 Lavista Rd, Tucker, GA 30084',
+    'lat': 33.846238,
+    'lng': -84.252318,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0285',
+    'phone': '(770) 414-5126',
+    'id': '5'
+  },
+  {
+    'neighborhood': 'Midtown',
+    'brand': 'Petsmart',
+    'address': '650 Ponce De Leon Ave NE, Atlanta, GA 30308',
+    'lat': 33.774076,
+    'lng': -84.365859,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0275',
+    'phone': '(404) 872-2363',
+    'id': '6'
+  },
+  {
+    'neighborhood': 'Norcross',
+    'brand': 'Petsmart',
+    'address': '6050 Peachtree Pkwy, Norcross, GA 30092',
+    'lat': 33.953883,
+    'lng': -84.232020,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=2111',
+    'phone': '(770) 300-0926',
+    'id': '7'
+  },
+  {
+    'neighborhood': 'Alpharetta',
+    'brand': 'Petsmart',
+    'address': '6370 North Point Pkwy, Alpharetta, GA 30022',
+    'lat': 34.046049,
+    'lng': -84.291594,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0289',
+    'phone': '(770) 343-8511',
+    'id': '8'
+  },
+  {
+    'neighborhood': 'Roswell',
+    'brand': 'Petsmart',
+    'address': '625 Crossville Rd Ste 134, Roswell, GA 30075',
+    'lat': 34.059709,
+    'lng': -84.381102,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=1048',
+    'phone': '(678) 352-8138',
+    'id': '9'
+  },
+  {
+    'neighborhood': 'Austell',
+    'brand': 'Petsmart',
+    'address': '4155 Austell Rd, Austell, GA 30106',
+    'lat': 33.851486,
+    'lng': -84.604122,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=2169',
+    'phone': '(678) 945-1081',
+    'id': '10'
+  },
+  {
+    'neighborhood': 'Petsmart',
+    'brand': 'Petsmart',
+    'address': '3803 Venture Dr, Duluth, GA 30096',
+    'lat': 33.953216,
+    'lng': -84.136986,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0283',
+    'phone': '(770) 813-8400',
+    'id': '11'
+  },
+  {
+    'neighborhood': 'Kennesaw',
+    'brand': 'Petsmart',
+    'address': '860 Cobb Place Blvd NW, Kennesaw, GA 30144',
+    'lat': 34.011621,
+    'lng': -84.571516,
+    'website': 'https://www.petsmart.com/store-locator/store/?storeID=0282',
+    'phone': '(770) 424-5226',
+    'id': '12'
+  },
+  {
+  'neighborhood': 'Woodstock',
+  'brand': 'Petsmart',
+  'address': '142 Woodstock Square Ave, Woodstock, GA 30189',
+  'lat': 34.084367,
+  'lng': -84.544430,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1029',
+  'phone': '(678) 445-2741',
+  'id': '13'
+},
+{
+  'neighborhood': 'Stone Mountain',
+  'brand': 'Petsmart',
+  'address': '2150 Paxton Dr SW, Lilburn, GA 30047',
+  'lat': 33.836862,
+  'lng': -84.075662,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0284',
+  'phone': '(770) 985-0469',
+  'id': '14'
+},
+{
+  'neighborhood': 'Johns Creek',
+  'brand': 'Petsmart',
+  'address': '"630 Peachtree Pkwy, Suwanee, GA 30024',
+  'lat': 34.071917,
+  'lng': -84.169927,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1356',
+  'phone': '(770) 814-8723',
+  'id': '15'
+},
+{
+  'neighborhood': 'East Point',
+  'brand': 'Petsmart',
+  'address': '3665 Marketplace Blvd, East Point, GA 30344',
+  'lat': 33.658637,
+  'lng': -84.503797,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1129',
+  'phone': '(404) 344-0773',
+  'id': '16'
+},
+{
+  'neighborhood': 'Snellville',
+  'brand': 'Petsmart',
+  'address': '1929 Scenic Highway N, Snellville, GA 30078',
+  'lat': 33.873427,
+  'lng': 33.873427,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=2348',
+  'phone': '(770) 225-0382',
+  'id': '17'
+},
+{
+  'neighborhood': 'Acworth',
+  'brand': 'Petsmart',
+  'address': '3335 Cobb Parkway NW Ste 500, Acworth, GA 30101',
+  'lat': 34.033837,
+  'lng': -84.683185,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1259',
+  'phone': '(770) 529-0748',
+  'id': '18'
+},
+{
+  'neighborhood': 'Lawrenceville',
+  'brand': 'Petsmart',
+  'address': '875 Lawrenceville-Suwanee Rd, Lawrenceville, GA 30043',
+  'lat': 33.970854,
+  'lng': -84.016913,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0280',
+  'phone': '(770) 995-2449',
+  'id': '19'
+},
+{
+  'neighborhood': 'Southlake',
+  'brand': 'Petsmart',
+  'address': '1986 Mount Zion Rd, Morrow, GA 30260',
+  'lat': 33.564435,
+  'lng': -84.325554,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0288',
+  'phone': '(770) 478-0860',
+  'id': '20'
+},
+{
+  'neighborhood': 'Hiram',
+  'brand': 'Petsmart',
+  'address': '4794 Jimmy Lee Smith Pkwy Ste 108, Hiram, GA 30141',
+  'lat': 33.886801,
+  'lng': -84.747018,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1075',
+  'phone': '(678) 567-0583',
+  'id': '21'
+},
+{
+  'neighborhood': 'Douglasville',
+  'brand': 'Petsmart',
+  'address': '2940 Chapel Hill Rd, Douglasville, GA 30135',
+  'lat': 33.726330,
+  'lng': -84.734072,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0276',
+  'phone': '(770) 942-3326',
+  'id': '22'
+},
+{
+  'neighborhood': 'Cumming',
+  'brand': 'Petsmart',
+  'address': '1020 Market Place Blvd, Cumming, GA 30041',
+  'lat': 34.180733,
+  'lng': -84.129354,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1049',
+  'phone': '(678) 513-1689',
+  'id': '23'
+},
+{
+  'neighborhood': 'Mall of Georgia',
+  'brand': 'Petsmart',
+  'address': '1705 Mall of Georgia Blvd 50, Buford, GA 30519',
+  'lat': 34.060325,
+  'lng': -83.994088,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0273',
+  'phone': '(678) 482-7449',
+  'id': '24'
+},
+{
+  'neighborhood': 'Canton',
+  'brand': 'Petsmart',
+  'address': '1810 Cumming Highway Ste 12, Canton, GA 30114',
+  'lat': 34.236810,
+  'lng': -84.458879,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1258',
+  'phone': '(770) 704-7400',
+  'id': '25'
+},
+{
+  'neighborhood': 'Loganville',
+  'brand': 'Petsmart',
+  'address': '4056 Atlanta Highway, Loganville, GA 30052',
+  'lat': 33.845833,
+  'lng': -83.910678,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1760',
+  'phone': '(678) 639-1960',
+  'id': '26'
+},
+{
+  'neighborhood': 'Conyers',
+  'brand': 'Petsmart',
+  'address': '1370 Dogwood Dr SE, Conyers, GA 30013',
+  'lat': 33.651777,
+  'lng': -84.000529,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0279',
+  'phone': '(770) 922-1772',
+  'id': '27'
+},
+{
+  'neighborhood': 'Fayetteville',
+  'brand': 'Petsmart',
+  'address': '101 Pavilion Pkwy, Fayetteville, GA 30214',
+  'lat': 33.480363,
+  'lng': -84.437276,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0281',
+  'phone': '(770) 719-4444',
+  'id': '28'
+},
+{
+  'neighborhood': 'McDonough',
+  'brand': 'Petsmart',
+  'address': '1950 Jonesboro Rd, McDonough, GA 30253',
+  'lat': 33.465574,
+  'lng': -84.220200,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1074',
+  'phone': '(678) 583-0287',
+  'id': '29'
+},
+{
+  'neighborhood': 'Cartersville',
+  'brand': 'Petsmart',
+  'address': '51 N Morningside Dr, Cartersville, GA 30121',
+  'lat': 34.168880,
+  'lng': -84.786474,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=2749',
+  'phone': '(678) 719-3021',
+  'id': '30'
+},
+{
+  'neighborhood': 'Griffin',
+  'brand': 'Petsmart',
+  'address': '1625 N Expressway, Griffin, GA 30223',
+  'lat': 33.275826,
+  'lng': -84.291586,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=2560',
+  'phone': '(770) 229-7122',
+  'id': '31'
+},
+{
+  'neighborhood': 'Flowery Branch',
+  'brand': 'Petsmart',
+  'address': '5873 Spout Springs Road, Flowery Branch, GA 30542',
+  'lat': 34.177754,
+  'lng': -83.912030,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1756',
+  'phone': '(770) 967-6664',
+  'id': '32'
+},
+{
+  'neighborhood': 'Winder',
+  'brand': 'Petsmart',
+  'address': '916 Loganville Hwy Bldg 1700, Bethlehem, GA 30620',
+  'lat': 33.943306,
+  'lng': -83.752244,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1759',
+  'phone': '(770) 307-4856',
+  'id': '33'
+},
+{
+  'neighborhood': 'Peachtree City',
+  'brand': 'Petsmart',
+  'address': '2701 W Hwy 54, Peachtree City, GA 30269',
+  'lat': 33.397029,
+  'lng': -84.596407,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=1757',
+  'phone': '(770) 631-5729',
+  'id': '34'
+},
+{
+  'neighborhood': 'Dawsonville',
+  'brand': 'Petsmart',
+  'address': '145 Forest Blvd Ste 300, Dawsonville, GA 30534',
+  'lat': 34.351889,
+  'lng': -84.044810,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=2731',
+  'phone': '(706) 216-2625',
+  'id': '35'
+},
+{
+  'neighborhood': 'Newnan',
+  'brand': 'Petsmart',
+  'address': '1072 Bullsboro Dr, Newnan, GA 30265',
+  'lat': 33.399248,
+  'lng': -84.744377,
+  'website': 'https://www.petsmart.com/store-locator/store/?storeID=0278',
+  'phone': '(770) 251-8086',
+  'id': '36'
+},
+{
+  'neighborhood': 'Midtown',
+  'brand': 'The Pet Set',
+  'address': '976 Piedmont Ave NE, Atlanta, GA 30309',
+  'lat': 33.781263,
+  'lng': -84.380975,
+  'website': 'https://thepetset.com/',
+  'phone': '(404) 249-6668',
+  'id': '37'
+},
+{
+  'neighborhood': 'Inman Park',
+  'brand': 'Inman Park Pet Works',
+  'address': '914 Austin Ave NE, Atlanta, GA 30307',
+  'lat': 33.762036,
+  'lng': -84.357464,
+  'website': 'http://www.inmanparkpetworks.com/',
+  'phone': '(404) 522-4544',
+  'id': '38'
+},
+{
+  'neighborhood': 'Virginia Highlands',
+  'brand': 'Highland Pet Supply',
+  'address': '1186 North Highland Avenue NE, Atlanta, GA 30306',
+  'lat': 33.787013,
+  'lng': -84.355976,
+  'website': 'http://www.highlandpet.com/',
+  'phone': '(404) 892-5900',
+  'id': '39'
+},
+{
+  'neighborhood': 'Virginia Highlands',
+  'brand': 'The Whole Dog Market',
+  'address': '985 Monroe Dr NE Ste D, Atlanta, GA 30308',
+  'lat': 33.781368,
+  'lng': -84.368277,
+  'website': 'https://www.thewholedogmarket.com/#featured',
+  'phone': '(404) 549-2727',
+  'id': '40'
+},
+{
+  'neighborhood': 'Brookhaven',
+  'brand': 'City Dog Market',
+  'address': '4244 Peachtree Rd NE, Atlanta, GA 30319',
+  'lat': 33.867190,
+  'lng': -84.336397,
+  'website': 'http://www.petsupplyatlanta.com/',
+  'phone': '(404) 816-8050',
+  'id': '41'
+},
+{
+  'neighborhood': 'Grant Park',
+  'brand': 'Intown Healthy Hound',
+  'address': '891 Cherokee Ave SE, Atlanta, GA 30315',
+  'lat': 33.730169,
+  'lng': -84.373882,
+  'website': 'https://www.intownhealthyhound.com/',
+  'phone': '(404) 228-3898',
+  'id': '42'
+},
+{
+  'neighborhood': 'Vinings',
+  'brand': 'Uptown Pups',
+  'address': '2355 Cumberland Pkwy SE #30, Atlanta, GA 30339',
+  'lat': 33.859473,
+  'lng': -84.480762,
+  'website': 'http://uptownpupsonline.com/',
+  'phone': '(678) 842-0041',
+  'id': '43'
+},
+{
+  'neighborhood': 'Sandy Springs',
+  'brand': 'The Pet People’s Store',
+  'address': '4920 Roswell Road, Suite 48-49, Atlanta, GA 30342',
+  'lat': 33.889808,
+  'lng': -84.383416,
+  'website': 'https://www.petpeoplestores.com/sandysprings.html',
+  'phone': '(404) 500-2653',
+  'id': '44'
+},
+{
+  'neighborhood': 'Smyrna',
+  'brand': 'Pets Marketplace',
+  'address': '2110 Paces Ferry Rd SE, Smyrna, GA 30080',
+  'lat': 33.864380,
+  'lng': -84.491664,
+  'website': 'https://www.petsmarketplc.com/',
+  'phone': '(404) 692-4240',
+  'id': '45'
+},
+{
+  'neighborhood': 'East Atlanta',
+  'brand': 'Park Pet Supply',
+  'address': '479 Flat Shoals Ave SE #A, Atlanta, GA 30316',
+  'lat': 33.740958,
+  'lng': -84.346548,
+  'website': 'http://parkpetsupply.com/',
+  'phone': '(404) 588-0140',
+  'id': '46'
+},
+{
+  'neighborhood': 'Roswell',
+  'brand': 'Top Dogs Pet Boutique',
+  'address': '900 Mansell Rd Ste 13, Roswell, GA 30076',
+  'lat': 34.043248,
+  'lng': -84.337980,
+  'website': 'https://www.topdogs.net/',
+  'phone': '(770) 641-8620',
+  'id': '47'
+},
+{
+  'neighborhood': 'Cartersville',
+  'brand': 'The Good Dog Shoppe',
+  'address': '5 S Public Square #201, Cartersville, GA 30120',
+  'lat': 34.165755,
+  'lng': -84.796550,
+  'website': 'http://thegooddogshoppe.com/',
+  'phone': '(770) 334-2524',
+  'id': '48'
+},
+{
+  'neighborhood': 'Kennesaw',
+  'brand': 'The Good Dog Shoppe',
+  'address': '4200 Wade Green Rd NW #5, Kennesaw, GA 30144',
+  'lat': 34.056258,
+  'lng': -84.590453,
+  'website': 'http://thegooddogshoppe.com/',
+  'phone': '(770) 919-0333',
+  'id': '49'
+},
+{
+  'neighborhood': 'Woodstock',
+  'brand': 'Sassy Paws Pet Boutique ',
+  'address': '1105 Parkside Ln, Woodstock, GA 30189',
+  'lat': 34.107711,
+  'lng': -84.553184,
+  'website': 'http://www.sassypawspb.com/',
+  'phone': '(678) 275-2126',
+  'id': '50'
+},
+{
+  'neighborhood': 'Roswell',
+  'brand': 'The Downtown Pooch',
+  'address': '1045 Canton St, Roswell, GA 30075',
+  'lat': 34.027262,
+  'lng': -84.360694,
+  'website': 'https://www.thedowntownpooch.com/',
+  'phone': '(678) 536-8471',
+  'id': '51'
+},
+{
+  'neighborhood': 'Marietta',
+  'brand': 'Bark Street Petopia',
+  'address': '136 S Park Square NE, Marietta, GA 30060',
+  'lat': 33.952148,
+  'lng': -84.549258,
+  'website': 'https://barkstreetpetopia.com/',
+  'phone': '(470) 485-2345',
+  'id': '52'
+},
+{
+  'neighborhood': 'Acworth',
+  'brand': 'Pet Suplies Plus',
+  'address': '6199 GA-92 #160, Acworth, GA 30102',
+  'lat': 34.090202,
+  'lng': -84.585500,
+  'website': 'https://www.petsuppliesplus.com/Store/GA/Acworth/Acworth/4083',
+  'phone': '(770) 672-6802',
+  'id': '55'
+},
+{
+  'neighborhood': 'Marietta',
+  'brand': 'Pet Suplies Plus',
+  'address': '"960 Shallowford Rd Ste 114-B, Marietta, GA 30066-3093',
+  'lat': 34.034804,
+  'lng': -84.466637,
+  'website': 'https://www.petsuppliesplus.com/Store/GA/Marietta/Marietta/8041',
+  'phone': '(770) 973-3551',
+  'id': '56'
+},
+{
+  'neighborhood': 'Smyrna',
+  'brand': 'Pet Suplies Plus',
+  'address': '3240 S Cobb Dr. SE Ste. 750, Smyrna, Ga 30080',
+  'lat': 33.875113,
+  'lng': -84.533618,
+  'website': 'https://www.petsuppliesplus.com/Store/GA/Smyrna/Smyrna/4020',
+  'phone': '(770) 431-0029',
+  'id': '57'
+},
+{
+  'neighborhood': 'Carrolton',
+  'brand': 'Pet Suplies Plus',
+  'address': '1301-C S Park St, Cartollton, Ga 30117',
+  'lat': 33.564622,
+  'lng': -85.077003,
+  'website': 'https://www.petsuppliesplus.com/Store/GA/Carrollton/Carrollton/8034',
+  'phone': '(770) 214-8825',
+  'id': '58'
+},
+{
+  'neighborhood': 'Sandy Springs',
+  'brand': 'Petco',
+  'address': '5938 Roswell Rd NE, Sandy Springs, GA 30328',
+  'lat': 33.917716,
+  'lng': -84.379309,
+  'website': 'https://stores.petco.com/ga/sandysprings/pet-supplies-sandysprings-ga-1729.html?ref=main-indy-button',
+  'phone': '(404) 255-6544',
+  'id': '59'
+},
+{
+'neighborhood': 'Marietta',
+'brand': 'Petco',
+'address': '1100 Johnson Ferry Road Suite 10, Marietta, GA 30068',
+'lat': 33.977420,
+'lng': -84.414462,
+'website': 'https://stores.petco.com/ga/marietta/pet-supplies-marietta-ga-1558.html?ref=main-indy-button',
+'phone': '(770) 321-3449',
+'id': '60'
+},
+{
+  'neighborhood': 'Buckhead',
+  'brand': 'Petco',
+  'address': '3756b Roswell Road, Atlanta, GA 30342',
+  'lat': 33.858147,
+  'lng': -84.381442,
+  'website': 'https://stores.petco.com/ga/atlanta/pet-supplies-atlanta-ga-1567.html?ref=main-indy-button',
+  'phone': '(404) 841-9733',
+  'id': '61'
+},
+{
+  'neighborhood': 'Kennesaw',
+  'brand': 'Petco',
+  'address': '1570 Crater Lake Dr Ste 300, Kennesaw, GA 30152',
+  'lat': 33.998955,
+  'lng': -84.591741,
+  'website': 'https://stores.petco.com/ga/kennesaw/pet-supplies-kennesaw-ga-2542.html?ref=main-indy-button',
+  'phone': '(770) 615-4147',
+  'id': '62'
+},
+{
+  'neighborhood': 'Marietta',
+  'brand': 'Petco',
+  'address': '3805 Dallas Highway SW Suite 340, Marietta, GA 30064',
+  'lat': 33.950202,
+  'lng': -84.663587,
+  'website': 'https://stores.petco.com/ga/marietta/pet-supplies-marietta-ga-2538.html?ref=main-indy-button',
+  'phone': '(770) 615-4070',
+  'id': '63'
+}
+
+];
+
+export default StoreLocations;
+
+const StoreNeighborhoods = [
+'Buckhead',
+ 'Perimeter', 
+ 'Smyrna', 
+ 'Castlegate', 
+ 'Marietta', 
+ 'Tucker', 
+ 'Midtown', 
+ 'Norcross', 
+ 'Alpharetta',
+ 'Roswell',
+ 'Austell',
+ 'Duluth',
+ 'Kennesaw',
+ 'Woodstock',
+ 'Stone Mountain',
+ 'Johns Creek',
+ 'East Point',
+ 'Snellville',
+ 'Acworth',
+ 'Lawrenceville',
+ 'Southlake',
+ 'Hiram',
+ 'Douglasville',
+ 'Cumming',
+ 'Mall of Georgia',
+ 'Canton',
+ 'Loganville',
+ 'Conyers',
+ 'Fayetteville',
+ 'McDonough',
+ 'Cartersville',
+ 'Griffin',
+ 'Flowery Branch',
+ 'Winder',
+ 'Peachtree City',
+ 'Dawsonville',
+ 'Newnan',
+ 'Inman Park',
+ 'Virginia Highlands',
+ 'Brookhaven',
+ 'Grant Park',
+ 'Sandy Springs',
+ 'East Atlanta',
+ 'Carrolton'
+ 
+];
+
+export default StoreNeighborhoods;
+
